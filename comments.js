130aa19c22dbc3e01b1c2f2696da412665a15125
wupengforIT <wupengforIT@163.com>
2018-12-29 18:57:45 +0800

    完成登录功能

--- comments.js
+++ comments.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = require('./router');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 app.engine('html', require('express-art-template'));
 app.set('views', './views');
 app.all('*', function (req, res, next) {
@@ -10,6 +15,14 @@ app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+app.use(cookieParser());
+app.use(session({
+        secret: '12345',
+        cookie: {maxAge: 60000},
+        resave: false,
+        saveUninitialized:false
+    }
+));
 app.use(router);
 app.use('/public/', express.static('./public/'));
 app.use('/node_modules/', express.static('./node_modules/'));
--- router.js
+++ router.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var DbUtils = require('./DbUtils');
-var fs = require('fs');
-var path = require('path');
+var utils = require('./util/utils');
 //访问管理后台首页
 router.get('/', function (req, res) {
-    /**
-     *  1、查询登录账号的权限
-     */
+    //1、判断此用户是否已经登录过
+    var user = utils.isLogin(req,res);
+    if(!user){
+        //session不存在，则需要直接返回登录界面
+        return;
+    }
     // 2、查询对应的菜单数据
     var sql = 'select * from mnues m where m.model_id = 1';
     DbUtils.queryData(sql,function (result) {
         for(var i = 0;i<result.length;i++){
-            addList(result,result[i]);
+            utils.addList(result,result[i]);
         }
-        // 将result中所有节点parent_id值不为空的给提出掉
+        // 将result中所有节点parent_id值不为空的给踢出掉
         var array = [];
         for(var i = 0;i<result.length;i++){
             if(!result[i]['parent_id']){
@@ -24,48 +26,29 @@ router.get('/', function (req, res) {
         var dataJsonArr = JSON.stringify(array);
         res.render('index.html',{dataJsonArr:dataJsonArr});
     });
-
-    function addList(result,obj) {
-        //判断当前节点是否有父节点
-        if(obj['parent_id']){
-            //如果有父节点，先找到父节点
-            for(var i = 0;i<result.length;i++){
-                if(obj['parent_id'] == result[i]['id']){
-                    //定位到父节点，先判断父节点中有没有sonList数组
-                    if(result[i]['sonList']){
-                        //如果有sonList，先判断sonList中有没有该子节点
-                        var index = 0;//定义一个索引记录子节点在父节点sonList中的索引
-                        var isContain = false;//定义一个变量记录父节点是否已经包含该子节点，默认不包含
-                        for(var j = 0;j<result[i]['sonList'].length;j++){
-                            if(result[i]['sonList'][j].id == obj.id){
-                                index == j;
-                                isContain = true;
-                                // 如果包含了该元素，再次判断,该元素是否发生过改变
-                                if(result[i]['sonList'][j].length != obj.length){
-                                    //发生过改变，重新替换父节点中对应的子节点
-                                    result[i]['sonList'][j] = obj;
-                                    //并且让父节点进一步通知其对应的父节点
-                                    addList(result,result[i]);
-                                }
-                                break;
-                            }
-                        }
-                        //如果父节点的sonList中不包含该子节点，则直接添加
-                        if(!isContain){
-                            result[i]['sonList'].push(obj);
-                        }
-                    }else{
-                        //如果没有sonList，先在父节点添加此数组
-                        result[i]['sonList'] = [];
-                        //父节点添加当前节点作为子节点
-                        result[i]['sonList'].push(obj);
-                        addList(result,result[i]);
-                    }
-                    break;
-                }
-            }
-        }
-    }
 });
-
+router.post('/baixiu/login',function (req,res) {
+    var emil = req.body.email;
+    var password = req.body.password;
+    console.log(emil+"======="+password);
+    var loginSql = 'select * from users u where u.`email` = "'+emil+'" and u.`password` = "'+password+'" ';
+    DbUtils.queryData(loginSql,function (result) {
+        var loginData = {};
+        if(result&&result.length > 0){
+            //登录成功，保存session
+            req.session.user = result;
+            //冲定向到主页
+            // res.redirect(301, '/');
+            loginData.login_state = '0';
+            loginData.login_desc = '成功';
+            loginData.user = result[0];
+        }else{
+            //未找到用户
+            loginData.login_state = '1';
+            loginData.login_desc = '失败';
+            loginData.user = null;
+        }
+        res.json(loginData);
+    });
+});
 module.exports = router;

ca6ce990b40b32f0b9deceb43225d006a761277f
wupeng <wupengforIT@163.com>
2019-01-07 21:28:19 +0800

    修改Dbutils

--- DbUtils.js
+++ DbUtils.js
@@ -1,20 +1,47 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+const db_config={
+    connectionLimit: 5,
+    host:"47.96.76.172",
+    user:"root",
+    password:"4217aBc!",
+    port:"3306",
+    database:"baixiu"
+};
+let pool=mysql.createPool(db_config);
+/*var connection = mysql.createConnection({
     host: '47.96.76.172',
     user: 'root',
     password: '4217aBc!',
     database: 'baixiu'
-});
-connection.connect();
+});*/
+// connection.connect();
 var Dbutils = {
-    queryData(sql, getResult) {
+    /*queryData(sql, getResult) {
         connection.query(sql, function (error, results, fields) {
             if (error) return getResult({});
             getResult(results);
+            // connection.end();
         });
-    },
-    closeDb() {
+    },*/
+    /*closeDb() {
         connection.end();
+    },*/
+    queryData(sql, getResult){
+        pool.getConnection(function(err,connect){//通过getConnection()方法进行数据库连接
+            if(err){
+                console.log(`mysql链接失败${err}`);
+            }else{
+                connect.query(sql,function(err,result){
+                    if(err){
+                        console.log(`SQL error:${err}`)
+                    }else{
+                        getResult(result);
+                        pool.releaseConnection(connect)//释放连接池中的数据库连接
+                        // pool.end();//关闭连接池
+                    }
+                });
+            }
+        });
     }
 };
 module.exports = Dbutils;

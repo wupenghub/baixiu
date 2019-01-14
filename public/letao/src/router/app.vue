6e4af887578269dbfb52746036c2907f265e7337
wupengforIT <wupengforIT@163.com>
2019-01-14 17:36:53 +0800

    完成app基本架构

--- public/letao/src/router/app.vue
+++ public/letao/src/router/app.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="container">
         <header>
-            <a class="mui-icon mui-icon-back"  href="javascript:window.history.back();">
-
-            </a>
+            <a class="mui-icon mui-icon-back"  href="javascript:window.history.back();"></a>
+            <span class="title">首页</span>
         </header>
     </div>
 </template>
@@ -30,8 +29,16 @@
             background-color: #39779C;
             a{
                 display: inline-block;
-                height: 100%;
+                height: 40px;
                 width: 50px;
+                line-height: 40px;
+                color: white;
+            }
+            .title{
+                display: inline-block;
+                text-align: center;
+                font-size: 14px;
+                color: white;
             }
         }
     }

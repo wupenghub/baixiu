2cace334991c0de8e3d0da851671d6d594c5a516
wupeng <wupengforIT@163.com>
2019-05-05 22:22:49 +0800

    同步代码

--- public/js/cis/cisApproval.js
+++ public/js/cis/cisApproval.js
@@ -1,6 +1,13 @@
-function downLoadTemplte() {
+$(function () {
+    $('#downLoad').click(function () {
+        var fileName = $('#desc').val()+'人员配置.xlsx';
+        downLoadTemplte(fileName);
+    });
+});
+function downLoadTemplte(fileName) {
     var url = "/baixiu/cisTemplateDownLoad";
-    var fileName = "人员收集模板.xls";
+    // var fileName = "人员收集模板.xls";
+    var fileName = fileName;
     var form = $("<form></form>").attr("action", url).attr("method", "post");
     form.append($("<input></input>").attr("type", "hidden").attr("name", "filename").attr("value", fileName));
     form.appendTo('body').submit().remove();
@@ -17,7 +24,7 @@ function uploadData() {
         processData: false,  // 不处理数据
         contentType:false,  // 不设置内容类型
         success:function(data){
-
+            console.log(data)
         },
         error:function(e){
             alert("错误！！");

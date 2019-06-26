5988c665b5618d19af5234cadec94ebd7d719cd7
wupengforIT <wupengforIT@163.com>
2019-06-26 14:45:10 +0800

    同步代码

--- public/js/bussnessTrip/CostType.js
+++ public/js/bussnessTrip/CostType.js
@@ -9,20 +9,52 @@ $(function () {
         //循环遍历集合元素,添加菜单目录。
         utils.addMnues(rootNode, dataJson.dataJsonArr[i]);
     }
-    $('#cost-list').on('click',function () {
+    $('#cost-list').on('click', function () {
         $('#cost_type_desc').val('');
         $('#cost_type_code').val('');
         $('#company_type_desc').val('');
         $('#company_type_code').val('');
         $('#level_desc').val('');
         $('#level_code').val('');
+        $('#cost_type_desc_modify').val('');
+        $('#cost_type_max_count_modify').val('');
+        $('.cost_type_div').hide();
     });
 
-    $('#cost-search').on('click',function () {
-        if(!$('#cost-no').val()){
-            alert('请输入订单编号');
+    $('#cost-search').on('click', function () {
+        if (!$('#cost_type_code').val() && !$('#cost_type_desc').val()) {
+            alert('请输入费用类型');
             return;
         }
+        if (!$('#company_type_desc').val() && !$('#company_type_code').val()) {
+            alert('请输入公司类型');
+            return;
+        }
+        if (!$('#level_desc').val() && !$('#level_code').val()) {
+            alert('请输入职级');
+            return;
+        }
+        utils.ajaxSend({
+            type: 'get',
+            url: '/baixiu/searchCostTypeInfo',
+            data: {
+                companyType: $('#company_type_code').val(),
+                costTypeCode: $('#cost_type_code').val(),
+                levelCode: $('#level_code').val()
+            },
+            dataType: "json"
+        }, function (data) {
+            console.log(data)
+            if(data.status == 0){
+                $('.cost_type_div').show();
+                $('#cost_type_desc_modify').val(data.returnData[0].costDesc);
+                $('#cost_type_max_count_modify').val(data.returnData[0].ceilCost);
+            }else{
+
+            }
+        }, function (error) {
+
+        });
     });
     //获取费用类型数据
     getCostTypeListData(1, utils.pageSize);
@@ -32,13 +64,13 @@ function getCostTypeListData(offset, pageSize) {
     utils.ajaxSend({
         type: 'get',
         url: '/baixiu/searchCostTypeList',
-        data: {offset,pageSize},
+        data: {offset, pageSize},
         dataType: "json"
-    },function (data) {
+    }, function (data) {
         console.log(data)
-        var html = template('costsList',data);
+        var html = template('costsList', data);
         $('.cost table tbody').html(html);
-        $('.cost_type_edit').on('click',function () {
+        $('.cost_type_edit').on('click', function () {
             $('.cost .cost-tabs li:first-child').removeClass('active');
             $('.cost .cost-tabs li:last-child').addClass('active');
             $('.cost .cost-content div:first-child').removeClass('active');
@@ -62,10 +94,10 @@ function getCostTypeListData(offset, pageSize) {
         //渲染分页页签
         returnData = data.returnData;
         data.returnData.totalCount = data.totalCount;
-        utils.pageList(data,$('.pages-nav'),function (currentPage,pageSize) {
+        utils.pageList(data, $('.pages-nav'), function (currentPage, pageSize) {
             getCostTypeListData(currentPage, pageSize);
         });
-    },function (error) {
+    }, function (error) {
 
     })
 }

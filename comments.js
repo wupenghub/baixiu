3b9b8c5344e7b5c13c3eacfca7f4117b2ab98117
wupengforIT <wupengforIT@163.com>
2019-07-25 18:35:05 +0800

    同步代码

--- comments.js
+++ comments.js
@@ -5,10 +5,11 @@ var service = require('./service');
 // var leTaoHomePage = require('./api/homePage.js');
 // var leTaoGoodDetail = require('./api/goodDetail.js');
 var leTaoLogin = require('./api/login.js');
-var cisApproval =  require('./api/cisApproval.js');
+var cisApproval = require('./api/cisApproval.js');
+var utils = require('./util/utils');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
@@ -28,9 +29,26 @@ app.use(session({
         secret: '12345',
         cookie: {maxAge: 6000000},
         resave: true,
-        saveUninitialized:false
+        saveUninitialized: false
     }
 ));
+app.use('/baixiu/', function (req, res, next) {
+    if (req.originalUrl == '/baixiu/isExitUser' || req.originalUrl == '/baixiu/forgetPwd' || req.originalUrl == '/baixiu/forgetPwd'
+        || req.originalUrl.indexOf('/baixiu/resetPwd') != -1 || req.originalUrl == '/baixiu/registered' || req.originalUrl == '/baixiu/registered'
+        || req.originalUrl == '/baixiu/register' || req.originalUrl == '/baixiu/login' || req.originalUrl == '/baixiu/pwdUpdate'
+        || req.originalUrl.indexOf('/baixiu/pwdReset') != -1
+        || req.originalUrl.indexOf('/baixiu/activated') != -1
+    ) {
+        next();
+        return;
+    }
+    var user = utils.isLogin(req, res);
+    if (!user) {
+        //session不存在，则需要直接返回登录界面
+        return;
+    }
+    next();  //路由继续向下匹配
+});
 /**
  * ===============================乐淘接口引入接口=====================================
  */
@@ -47,7 +65,6 @@ app.use(cisApproval);
 app.use('/public/', express.static('./public/'));
 app.use('/node_modules/', express.static('./node_modules/'));
 app.use('/uploads/', express.static('./uploads/'));
-
 app.listen(3000, function () {
     console.log('Running...');
 

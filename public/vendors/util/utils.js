c477386e3fe4a65bf59dd879bbd36b94365f9387
wupeng <wupengforIT@163.com>
2019-08-04 16:02:22 +0800

    同步代码

--- public/vendors/util/utils.js
+++ public/vendors/util/utils.js
@@ -311,9 +311,11 @@ var utils = {
         var rootNode = $('.aside .nav');
         $('.avatar').prop('src', dataJson.user.avatar);
         $('.name').html(dataJson.user.nickname);
-        for (var i = 0; i < dataJson.dataJsonArr.length; i++) {
-            //循环遍历集合元素,添加菜单目录。
-            utils.addMnues(rootNode, dataJson.dataJsonArr[i]);
+        if(dataJson.dataJsonArr) {
+            for (var i = 0; i < dataJson.dataJsonArr.length; i++) {
+                //循环遍历集合元素,添加菜单目录。
+                utils.addMnues(rootNode, dataJson.dataJsonArr[i]);
+            }
         }
         fun(dataJson);
     },

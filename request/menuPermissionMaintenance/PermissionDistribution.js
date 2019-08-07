cd457ed1b23c36e8c659a845314d616f9929418a
wupengforIT <wupengforIT@163.com>
2019-08-07 16:38:48 +0800

    同步代码

--- request/menuPermissionMaintenance/PermissionDistribution.js
+++ request/menuPermissionMaintenance/PermissionDistribution.js
@@ -6,7 +6,6 @@ var PermissionDistribution = {
         utils.renderPage(req,res,'permissionDistribution.html');
     },
     getUserListData(req,res){
-        console.log(req.query.email+'==='+req.query.nickName)
         var returnObj = {};
         returnObj.returnData = {
             offset: req.query.offset,
@@ -42,6 +41,46 @@ var PermissionDistribution = {
             returnObj.getlist_desc = error;
             res.json(returnObj);
         });
+    },
+    searchUserPerList(req,res){
+        var userExist = PermissionDistributionSql.userExistSql(req.query.email);
+        //查询权限清单
+        DbUtils.queryData(userExist,function (result) {
+            if(parseInt(result[0].count) == 0){
+                res.json({
+                    status:1,
+                    desc:'未查询到此用户'
+                });
+            }else{
+                var returnData = {};
+                var queryUserInfo = PermissionDistributionSql.queryUserInfo(req.query.email);
+                returnData.status = 3;
+                returnData.desc = '未知错误';
+                console.log('searchUserPerList查询人员菜单权限：'+queryUserInfo);
+                DbUtils.queryData(queryUserInfo,function (result) {
+                    returnData.mnueJsonArray = result;
+                    var queryCompanySql = PermissionDistributionSql.userCompanySql(req.query.email);
+                     DbUtils.queryData(queryCompanySql,function (result) {
+                         returnData.companyJsonArray = result;
+                         returnData.status = 0;
+                         res.json(returnData);
+                     },function (error) {
+                         returnData.status = -1;
+                         returnData.desc = error;
+                         res.json(returnData);
+                     });
+
+                },function (error) {
+                    returnData.status = -1;
+                    returnData.desc = error;
+                    res.json(returnData);
+                });
+            }
+        },function (error) {
+            returnData.status = -1;
+            returnData.desc = error;
+            res.json(returnData);
+        })
     }
 };
 module.exports = PermissionDistribution;
